dae4be46cceadaaad994d4a17bddbc9b098cc6c5
FastomGel <amogus.susus.sus.666zuz@gmail.com>
2026-09-13 19:14:34 +0600

    Player movement has been fully implemented, work on interacting with nightstands has begun, and the spinning cheese has been removed :(

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,96 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int speed = 10;
+    [SerializeField] private int moveSpeed = 7;
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
 
     void Update()
     {
-        // Собираем направление движения
-        Vector3 moveDirection = Vector3.zero;
+        Vector2 inputVector = gameInput.GetMovementVectorNormilized();
 
-        if (Input.GetKeyDown(KeyCode.W))
-            moveDirection += Vector3.forward;
-        if (Input.GetKeyDown(KeyCode.S))
-            moveDirection += Vector3.back;
-        if (Input.GetKeyDown(KeyCode.A))
-            moveDirection += Vector3.left;
-        if (Input.GetKeyDown(KeyCode.D))
-            moveDirection += Vector3.right;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        // Нормализация для равномерной скорости
-        if (moveDirection.magnitude > 1f)
-            moveDirection.Normalize();
-
-        // Поворот в направлении движения
-        if (moveDirection != Vector3.zero)
+        float moveDistance = moveSpeed * Time. deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 1.9f;
+        bool canMove = !Physics.CapsuleCast(transform.position,
+                                            transform.position + Vector3.up * playerHeight,
+                                            playerRadius,
+                                            moveDir,
+                                            moveDistance);
+        if (!canMove)
         {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
+            Vector2 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position,
+                                            transform.position + Vector3.up * playerHeight,
+                                            playerRadius,
+                                            moveDirX,
+                                            moveDistance);
+
+            if(canMove)
+            {
+                //Can move  only on the X
+                moveDir = moveDirX;
+                
+            }
+            else
+            {
+                //Cant move only on the X
+                //Attempt only Z movement
+
+                Vector2 moveDirZ = new Vector3(moveDir.x, 0, 0).normalized;
+                canMove = !Physics.CapsuleCast(transform.position,
+                                                transform.position + Vector3.up * playerHeight,
+                                                playerRadius,
+                                                moveDirZ,
+                                                moveDistance);
+                if(canMove)
+                {
+                    //Can move only on the Z
+                    moveDir = moveDirZ;
+                    
+                }
+                else
+                {
+                    //Cannot move in any direction
+                }
+
+            }
+        }
+        if(canMove)
+        {
+            transform.position += moveDir * moveDistance;
         }
 
-        // Перемещение с учетом скорости и времени
-        transform.position += moveDirection * speed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormilized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance))
+        {
+            Debug.Log(raycastHit.transform);
+        }
+        else
+        {
+            Debug.Log("-");
+        }
     }
 }
